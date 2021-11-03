0bf296d5d76b4fc8e55d3c909ef56555f92fb49b
Vitaly <vit@bb-tech.eu>
2021-11-03 20:17:55 +0200

    Classwork and first commit

---
+++ Classwork/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // neavr izveidot , jo Vehicle ir abstract classe
+            // var vehicle = new Vehicle();
+            // vehicle.Name = "Audi";
+            // vehicle.IncreaseSpeed();
+
+
+            var car = new Car();
+            car.Name = "BMW";
+            car.IncreaseSpeed();
+            car.StartToDrive();
+
+            var ship = new Ship();
+            ship.Name = "Baltica";
+            ship.IncreaseSpeed();
+            ship.SetSail();
+
+            SoundHorn3Times(car);
+            SoundHorn3Times(ship);
+
+            
+
+
+            Console.ReadLine();
+
+        }
+
+        public static void SoundHorn3Times(Vehicle vehicle) {
+            vehicle.SoundHorn();
+            vehicle.SoundHorn();
+            vehicle.SoundHorn();
+            //can call
+            vehicle.StartMoving();
+
+
+        }
+    }
+}
---
+++ Classwork/Vehicle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork
+{
+    public abstract class Vehicle
+    {
+        public string Name { get; set; }
+        public abstract void StartMoving();
+        public void IncreaseSpeed()
+        {
+
+        }
+        public virtual void SoundHorn() {
+            Console.WriteLine("beep beep");
+        }
+
+    }
+
+
+    public class Car : Vehicle 
+    {
+        public override void StartMoving() {
+            Console.WriteLine("Car is starting to move");
+        }
+        public void StartToDrive() 
+        { 
+        
+        }
+
+    }
+
+    public class Ship : Vehicle
+    {
+
+        public override void StartMoving()
+        {
+            Console.WriteLine("Ship  is setting sale");
+        }
+        public void SetSail()
+        {
+
+        }
+
+        public override void SoundHorn()
+        {
+            Console.WriteLine("Tuuu tuu");
+        }
+    }
+}
